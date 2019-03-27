006f4627bfdf3ffddfb8b85275509032a9096e60
Lucas Silva <lucas.nascimento@gmail.com>
2019-03-26 22:04:20 -0700

    Final commit before adding commands (hopefully)

--- src/index.js
+++ src/index.js
@@ -10,11 +10,16 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
+    client.user.setActivity(`with my dick ${process.env.TOKEN}help`);
 });
 
 client.on("message", msg => {
     if(msg.author.bot) return;
     handler.execute(msg);
+});
+
+client.on("error", error => {
+    console.error(error);
 });
 
 client.login(process.env.API_KEY);
